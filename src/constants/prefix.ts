079803c3eb95566c08eafb304c2679d2719ddfd9
alexbgh1 <alex.garnica@alumnos.uach.cl>
2024-10-03 00:38:43 -0300

    [fix]: audio still not working

--- src/constants/prefix.ts
+++ src/constants/prefix.ts
@@ -2,6 +2,6 @@
  * This file contains the prefix for the frontend, should be the same as the one in the vite.config.ts
  * The value is used on deployment to determine the path of the frontend
  */
-const FRONTEND_PREFIX = "minireplace-frontend";
+// const FRONTEND_PREFIX = "minireplace-frontend";
 
-export { FRONTEND_PREFIX };
+// export { FRONTEND_PREFIX };
